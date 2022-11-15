36cfab2de32723a92255437c3d768740a2575517
Victor <VictorCarmona9035@hotmail.com>
2022-11-15 18:55:50 +0100

    3-Migracion hacia SQL Server

--- BackEnd/BackEnd/Startup.cs
+++ BackEnd/BackEnd/Startup.cs
@@ -1,5 +1,7 @@
+using BackEnd.Persistence.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,6 +24,10 @@ namespace BackEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AplicationDbContext>(options =>
+            {
+            options.UseSqlServer(Configuration.GetConnectionString("Conexion"));
+            });
             services.AddControllersWithViews();
         }
 
