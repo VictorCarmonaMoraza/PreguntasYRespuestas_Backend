1a472e26ea12dbcec67651b316a2602ec3dfa40b
Victor <VictorCarmona9035@hotmail.com>
2022-11-15 18:34:14 +0100

    2-Instalacion de dependencias y creacion de AplicationDbContext

--- BackEnd/BackEnd/Domain/Models/Usuario.cs
+++ BackEnd/BackEnd/Domain/Models/Usuario.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,12 @@ namespace BackEnd.Domain.Models
 {
     public class Usuario
     {
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName ="varchar(20)")]
+        public string NombreUsuario { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string Password { get; set; }
     }
 }
