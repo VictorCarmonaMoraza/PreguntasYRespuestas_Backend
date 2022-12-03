631fea55bb8f92a2a2d187a1566a570ac37ea627
Victor <VictorCarmona9035@hotmail.com>
2022-12-03 19:30:37 +0100

    21-Creacion de interfaces, servicio y repositorio del Cuestionario

---
+++ BackEnd/BackEnd/Persistence/Repositories/CuestionarioRepository.cs
@@ -0,0 +1,26 @@
+﻿using BackEnd.Domain.IRepositories;
+using BackEnd.Domain.Models;
+using BackEnd.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Persistence.Repositories
+{
+    public class CuestionarioRepository: ICuestionarioRepository
+    {
+        private readonly AplicationDbContext _context;
+        //Constructor
+        public CuestionarioRepository(AplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task CreateCuestionario(Cuestionario cuestionario)
+        {
+            _context.Add(cuestionario);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ BackEnd/BackEnd/Services/CuestionarioService.cs
@@ -0,0 +1,21 @@
+﻿using BackEnd.Domain.IRepositories;
+using BackEnd.Domain.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace BackEnd.Services
+{
+    public class CuestionarioService : ICuestionarioRepository
+    {
+        private readonly ICuestionarioRepository _cuestionarioRepository;
+        public CuestionarioService(ICuestionarioRepository cuestionarioService)
+        {
+            this._cuestionarioRepository = cuestionarioService;
+        }
+
+        public async Task CreateCuestionario(Cuestionario cuestionario)
+        {
+            await _cuestionarioRepository.CreateCuestionario(cuestionario);
+        }
+    }
+}
