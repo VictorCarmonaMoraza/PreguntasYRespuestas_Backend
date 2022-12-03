0b8b814afd186de87a7125e8901acc33a8b901ee
Victor <VictorCarmona9035@hotmail.com>
2022-12-03 14:52:57 +0100

    20-Configuracion de Clases Parte 2: Migracion hacia BBDD

--- BackEnd/BackEnd/Domain/Models/Pregunta.cs
+++ BackEnd/BackEnd/Domain/Models/Pregunta.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,17 @@ namespace BackEnd.Domain.Models
 {
     public class Pregunta
     {
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName ="varchar(100)")]
+        public string Descripcion { get; set; }
+
+        //Clave foranea
+        public int CuestionarioId { get; set; }
+        //Propiedades de navegacion
+        public Cuestionario Cuestionario { get; set; }
+
+        public List<Respuesta> Respuesta { get; set; }
     }
 }
--- BackEnd/BackEnd/Domain/Models/Respuesta.cs
+++ BackEnd/BackEnd/Domain/Models/Respuesta.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,19 @@ namespace BackEnd.Domain.Models
 {
     public class Respuesta
     {
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName ="varchar(50)")]
+        public string Descripcion { get; set; }
+
+        [Required]
+        public bool EsCorrecta { get; set; }
+
+        //Relacion foranea de preguntas a respuestas
+        public int PreguntaId { get; set; }
+
+        //Navegacion
+        public Pregunta Pregunta { get; set; }
     }
 }
